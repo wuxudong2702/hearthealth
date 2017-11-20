838895330f35197ae4833cb161635e38ff42ac74
wuxudong <2713391431@qq.com>
2017-11-20 10:53:43 +0800

    log,admin-role,admin-user 排序，分页，查询完成； admin-role添加提示以及跳转页面待完成； 修改树节点json文件

--- src/app/business/admin-auth/admin-role/admin-role.component.ts
+++ src/app/business/admin-auth/admin-role/admin-role.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {cell, SortDirection, sortObj,DataType,INPUTTYPE} from '../../../shared/table/table-list.component';
+import {cell, SortDirection, sortObj,DataType,INPUTTYPE,searchObj,paginationObj} from '../../../shared/table/table-list.component';
 import {ApiService} from '../../../business-service/api/api.service';
 import 'rxjs/add/operator/toPromise';
+import {ToastService} from '../../../shared/toast/toast.service';
+import {ToastConfig, ToastType} from '../../../shared/toast/toast-model';
+import {ModalService} from '../../../shared/modal/modal.service';
+import {ConfirmConfig} from '../../../shared/modal/modal-model';
 
 @Component({
   selector: 'app-admin-role',
@@ -11,10 +15,29 @@ import 'rxjs/add/operator/toPromise';
 })
 export class AdminRoleComponent implements OnInit {
 
-  constructor(private http: ApiService) {}
+  constructor(private http: ApiService,private modalService: ModalService,private toastService: ToastService) {}
 
   ngOnInit() {
-    this.headers= this.http.getHeader('roles');
+      this.headers= this.http.getHeader('roles');
+
+      this.getHeartData(this.url);
+      console.log(this.headers, this.data);
+      this.http.isHavePerm('admin-role-del').then(v => {
+          this.deleteBtn = v;
+          this.deleteAllBtn = v;
+      });
+      this.http.isHavePerm('admin-role-edit').then(v => {
+          this.editZTreeBtn = v;
+      });
+      this.http.isHavePerm('admin-role-add').then(v => {
+          this.addBtn = v;
+      });
+     //  this.http.getAdminRoleHeader().then(data => {
+     //      this.headers = data['headers'];
+     //  });
+     //  this.http.getAdminRoleData().then(data => {
+     //      this.data = data['data'];
+     //  });
   }
 
   nodes:any;
@@ -22,20 +45,35 @@ export class AdminRoleComponent implements OnInit {
   data: Array<any> =[];
   headerAdd: Array<cell> = [];
 
-  adminRoleDel: boolean = false;//this.http.isHavePerm('admin-role-del');
-  adminRoleAdd: boolean = false;// this.http.isHavePerm('admin-role-add');
-  adminRoleEdit: boolean =  false;//this.http.isHavePerm('admin-role-edit');
-  deleteBtn: boolean = this.adminRoleDel;
-  deleteAllBtn: boolean = this.adminRoleDel;
-  addBtn: boolean = this.adminRoleAdd;
-  editZTreeBtn: boolean = this.adminRoleEdit;
+  // adminRoleDel: boolean = false;//this.http.isHavePerm('admin-role-del');
+  // adminRoleAdd: boolean = false;// this.http.isHavePerm('admin-role-add');
+  // adminRoleEdit: boolean =  false;//this.http.isHavePerm('admin-role-edit');
+  // deleteBtn: boolean = this.adminRoleDel;
+  // deleteAllBtn: boolean = this.adminRoleDel;
+  // addBtn: boolean = this.adminRoleAdd;
+  // editZTreeBtn: boolean = this.adminRoleEdit;
+  deleteBtn: boolean = false;
+  deleteAllBtn: boolean = false;
+  addBtn: boolean = false;
+  editZTreeBtn: boolean = false;
+  paginationBtn: boolean = true;
+  searchBtn: boolean = true;
+  setBtn: boolean = true;
 
   setOperate: boolean = true;
   editId: number;
-  addEditTitle: string = '添加';
+  addEditTitle: string = '';
   tableView: boolean = true;
   addView: boolean = false;
   editView: boolean = false;
+
+  pagination: paginationObj = new paginationObj();
+  per_page: string=null;
+  find_key: string=null;
+  find_val: string=null;
+  sort_key: string=null;
+  sort_val: string=null;
+  url: string = '';
 
   editZTree(id:number) {
     this.http.getZtreeNodes().then(data => {
@@ -70,12 +108,6 @@ export class AdminRoleComponent implements OnInit {
          this.data=data['data'];
      });
   }
-  sort(sort: sortObj) {
-     this.http.postAdminRoleSort(sort.key,sort.val).then(data=>{
-         console.log(data,'排序');
-         this.data=data['data'];
-     });
-  }
 
   cancel() {
      this.addView = false;
@@ -86,7 +118,17 @@ export class AdminRoleComponent implements OnInit {
   submit(submitData: string) {
      this.http.postAdminRoleSubmit(submitData).then(data => {
          console.log(data, '提交');
+         console.log(submitData, '喵');
          this.data = data['data'];
+
+         let confirmCfg = new ConfirmConfig('是否配置权限？');
+         let result = this.modalService.confirm(confirmCfg);
+         result.then(v => {
+             // this.editZTree(submitData.id);
+         }).catch(v => {
+
+         });
+
          this.addView = false;
          this.editView=false;
          this.tableView = true;
@@ -104,5 +146,55 @@ export class AdminRoleComponent implements OnInit {
      this.tableView = true;
      this.addView=false;
      this.editView=false;
+  }
+
+  getHeartData(url: string = this.url, per_page: string = this.per_page, find_key: string = this.find_key, find_val: string = this.find_val, sort_key: string = this.sort_key, sort_val: string = this.sort_val) {
+        this.http.getData(url, per_page, find_key, find_val, sort_key, sort_val).then(data => {
+            if (data['status'] == 'ok') {
+                this.data = data['data']['data'];
+                this.pagination.current_page = data['data']['current_page'];
+                this.pagination.last_page = data['data']['last_page'];
+                this.pagination.per_page = data['data']['per_page'];
+                this.pagination.total = data['data']['total'];
+                this.pagination.first_page_url = data['data']['first_page_url'];
+                this.pagination.last_page_url = data['data']['last_page_url'];
+                this.pagination.next_page_url = data['data']['next_page_url'];
+                this.pagination.prev_page_url = data['data']['prev_page_url'];
+                this.pagination.to = data['data']['to'];
+                // console.log(this.pagination,'pagination======');
+            } else {
+                const toastCfg = new ToastConfig(ToastType.ERROR, '', data.message, 3000);
+                this.toastService.toast(toastCfg);
+            }
+        }).catch(err => {
+            const toastCfg = new ToastConfig(ToastType.ERROR, '', err, 3000);
+            this.toastService.toast(toastCfg);
+        });
+  }
+
+  paginationChange(parmas) {
+        this.per_page = parmas['per_page'];
+        if(parmas['url']!=undefined){
+            this.url = parmas['url'];
+        }
+        this.getHeartData( this.url, this.per_page, this.find_key, this.find_val, this.sort_key, this.sort_val);
+  }
+
+  sort(sort: sortObj) {
+      this.sort_key = sort.key;
+      this.sort_val = sort.val;
+      this.getHeartData(this.url, this.per_page, this.find_key, this.find_val, this.sort_key, this.sort_val);
+  }
+
+  set (set: string) {
+      this.http.setHeader('roles', set).then(v => v).then(w => {
+          this.headers = this.http.getHeader('roles');
+      });
+  }
+
+  search(searchObj: searchObj) {
+      this.find_val = searchObj.searchValue;
+      this.find_key = searchObj.selectValue;
+      this.getHeartData(this.url, this.per_page, this.find_key, this.find_val, this.sort_key, this.sort_val);
   }
 }
--- src/app/business/admin-auth/admin-user/admin-user.component.ts
+++ src/app/business/admin-auth/admin-user/admin-user.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {cell, SortDirection, sortObj,DataType,INPUTTYPE} from '../../../shared/table/table-list.component';
+import {cell, SortDirection, sortObj,DataType,INPUTTYPE,searchObj,paginationObj} from '../../../shared/table/table-list.component';
 import {HttpClient} from "@angular/common/http";
 import {ApiService} from '../../../business-service/api/api.service';
 import 'rxjs/add/operator/toPromise';
+import {ToastService} from '../../../shared/toast/toast.service';
+import {ToastConfig, ToastType} from '../../../shared/toast/toast-model';
+import {ConfirmConfig} from '../../../shared/modal/modal-model';
 
 @Component({
   selector: 'app-admin-user',
@@ -13,11 +16,22 @@ import 'rxjs/add/operator/toPromise';
 
 export class AdminUserComponent implements OnInit {
 
-  constructor(private http: ApiService) {}
+  constructor(private http: ApiService,private toastService: ToastService) {}
 
   ngOnInit() {
     this.headers= this.http.getHeader('admins');
-
+    this.getHeartData(this.url);
+    console.log(this.headers, this.data);
+    this.http.isHavePerm('admin-user-del').then(v => {
+        this.deleteBtn = v;
+        this.deleteAllBtn = v;
+    });
+    this.http.isHavePerm('admin-user-edit').then(v => {
+        this.editBtn = v;
+    });
+    this.http.isHavePerm('admin-user-add').then(v => {
+        this.addBtn = v;
+    });
   }
   nodes:any;
   headers: Array<cell> = [];
@@ -26,17 +40,32 @@ export class AdminUserComponent implements OnInit {
   addEditTitle: string = '添加';
   editId: number;
 
-  adminUserDel: boolean =false;// this.http.isHavePerm('admin-user-del');
-  adminUserAdd: boolean = false;//this.http.isHavePerm('admin-user-add');
-  adminUserEdit: boolean = false;//this.http.isHavePerm('admin-user-edit');
-  deleteBtn: boolean = this.adminUserDel;
-  deleteAllBtn: boolean = this.adminUserDel;
-  addBtn: boolean = this.adminUserAdd;
-  editBtn: boolean = this.adminUserEdit;
+  // adminUserDel: boolean =false;// this.http.isHavePerm('admin-user-del');
+  // adminUserAdd: boolean = false;//this.http.isHavePerm('admin-user-add');
+  // adminUserEdit: boolean = false;//this.http.isHavePerm('admin-user-edit');
+  // deleteBtn: boolean = this.adminUserDel;
+  // deleteAllBtn: boolean = this.adminUserDel;
+  // addBtn: boolean = this.adminUserAdd;
+  // editBtn: boolean = this.adminUserEdit;
+  deleteBtn: boolean = false;
+  deleteAllBtn: boolean = false;
+  addBtn: boolean = false;
+  editBtn: boolean = false;
   setOperate: boolean = true;
+  paginationBtn: boolean = true;
+  searchBtn: boolean = true;
+  setBtn: boolean = true;
 
   tableView: boolean = true;
   addView: boolean = false;
+
+  pagination: paginationObj = new paginationObj();
+  per_page: string=null;
+  find_key: string=null;
+  find_val: string=null;
+  sort_key: string=null;
+  sort_val: string=null;
+  url: string = '';
 
   onDel(id:number){
       this.http.postAdminUserDel(id).then(data=>{
@@ -97,6 +126,56 @@ export class AdminUserComponent implements OnInit {
       });
       this.addView = false;
       this.tableView = true;
+  }
+
+    getHeartData(url: string = this.url, per_page: string = this.per_page, find_key: string = this.find_key, find_val: string = this.find_val, sort_key: string = this.sort_key, sort_val: string = this.sort_val) {
+        this.http.getData(url, per_page, find_key, find_val, sort_key, sort_val).then(data => {
+            if (data['status'] == 'ok') {
+                this.data = data['data']['data'];
+                this.pagination.current_page = data['data']['current_page'];
+                this.pagination.last_page = data['data']['last_page'];
+                this.pagination.per_page = data['data']['per_page'];
+                this.pagination.total = data['data']['total'];
+                this.pagination.first_page_url = data['data']['first_page_url'];
+                this.pagination.last_page_url = data['data']['last_page_url'];
+                this.pagination.next_page_url = data['data']['next_page_url'];
+                this.pagination.prev_page_url = data['data']['prev_page_url'];
+                this.pagination.to = data['data']['to'];
+                // console.log(this.pagination,'pagination======');
+            } else {
+                const toastCfg = new ToastConfig(ToastType.ERROR, '', data.message, 3000);
+                this.toastService.toast(toastCfg);
+            }
+        }).catch(err => {
+            const toastCfg = new ToastConfig(ToastType.ERROR, '', err, 3000);
+            this.toastService.toast(toastCfg);
+        });
+    }
+
+    paginationChange(parmas) {
+        this.per_page = parmas['per_page'];
+        if(parmas['url']!=undefined){
+            this.url = parmas['url'];
+        }
+        this.getHeartData( this.url, this.per_page, this.find_key, this.find_val, this.sort_key, this.sort_val);
+    }
+
+    sort(sort: sortObj) {
+        this.sort_key = sort.key;
+        this.sort_val = sort.val;
+        this.getHeartData(this.url, this.per_page, this.find_key, this.find_val, this.sort_key, this.sort_val);
+    }
+
+    set (set: string) {
+        this.http.setHeader('users', set).then(v => v).then(w => {
+            this.headers = this.http.getHeader('users');
+        });
+    }
+
+    search(searchObj: searchObj) {
+        this.find_val = searchObj.searchValue;
+        this.find_key = searchObj.selectValue;
+        this.getHeartData(this.url, this.per_page, this.find_key, this.find_val, this.sort_key, this.sort_val);
     }
 
 }

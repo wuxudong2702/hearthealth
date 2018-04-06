b6363e1d2ba344aa5d65bfa5c7f17188b712a775
wuxudong <2713391431@qq.com>
2018-04-06 12:24:12 +0800

    咋回事

--- src/app/business/dev/dev.component.ts
+++ src/app/business/dev/dev.component.ts
@@ -115,7 +115,6 @@ export class DevComponent implements OnInit {
 
   set (set: string) {
     console.log("set",set);
-      // "type,true,2;mobile,true,3;name,true,0;dev_id,true,5;qq,true,6;weixin,true,7;"
     this.http.setHeader('devs',set ).then(v => v).then(w => {
       this.headers = this.http.getHeader('devs');
     });

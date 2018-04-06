db6b8b1d5e2692d5e670b2d3f895f387e88dbb3c
wuxudong <2713391431@qq.com>
2018-04-06 11:08:51 +0800

    删除了很多没用的文件

--- src/app/shared/image-viewer/image-viewer.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImageViewerDirective } from './image-viewer.directive';
-
-describe('ImageViewerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImageViewerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/shared/img-select/img-select.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImgSelectDirective } from './img-select.directive';
-
-describe('ImgSelectDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImgSelectDirective();
-    expect(directive).toBeTruthy();
-  });
-});
